4cb79f125f4c1614504458be3048ab7749f5754c
Franckcracko <brianleonardomtzcoboswigueta@gmail.com>
2026-06-17 15:31:08 -0600

    feature: update tailwind and add support shadcn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
   extends: [
     'standard',
     'plugin:react/recommended'
   ],
+  ignorePatterns: ['dist', 'node_modules', '*.config.js'],
+  settings: {
+    react: { version: 'detect' }
+  },
   overrides: [
     {
       env: {
         node: true
       },
       files: [
-        '.eslintrc.{js,cjs}'
+        '.eslintrc.{js,cjs}',
+        'vite.config.js',
+        'postcss.config.js'
       ],
       parserOptions: {
         sourceType: 'script'
@@ -22,7 +29,8 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
   },
   plugins: [
     'react'
